08b0a55530ae4c373ff7251c4d91768ef51b8fcb
Arshpreet Singh <arshpreet.singh@jungleworks.com>
2026-06-02 01:40:52 +0530

    hollow diagonal done

--- Star Pattern/hollowDiagonalSquare.js
+++ Star Pattern/hollowDiagonalSquare.js
@@ -1,20 +1,38 @@
-let i , j;
+// let i, j;
+// let size = 10;
+// for (i = 1; i <= size; i++) {
+//     let row = ''
+//     for (j = 1; j <= size; j++) {
+//         if (i == 1 || i == size || j == 1 || j == size) {
+//             row += "*";
+//         }
+//         else {
+//             row += ' '
+//         }
+//         if (i == j) {
+//             row += '*'
+//         }
+//         if (i + j == size + 1) {
+//             row += '*'
+//         }
+//     }
+//     console.log(row);
+// }
+
+// making a hollow diagonal square 
 let size = 10;
-for(i = 1; i<= size ; i++){
-    let row = ''
-    for(j= 1; j <= size; j++){
-        if(i==1 || i==size || j==1 || j==size){
+for (let i = 1; i <= size; i++) {
+    let row = "";
+    for (let j = 1; j <= size; j++) {
+
+        if (i === 1 || i === size || j === 1 || j === size) {
             row += "*";
+        } else {
+            row += " ";
         }
-        else {
-        row += ' '
-        }
-        if(i==j){
-            row += '*'
-        }
-        if(i+j == size + 1){
-            row += '*'
+        if (i == j || i + j === size + 1) {
+            row += "*"
         }
     }
-    console.log(row);    
+    console.log(row);
 }
